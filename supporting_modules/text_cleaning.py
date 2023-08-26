d6e1fa159c49f4afc220bb59fd9806e6d1127cb4
KonradKaczmarek79 <konrad.kaczmarek79@gmail.com>
2023-08-26 15:57:55 +0200

    clean html tags

--- supporting_modules/text_cleaning.py
+++ supporting_modules/text_cleaning.py
@@ -1,6 +1,10 @@
 import re
 from collections import Counter
 
+PATTERNS = {
+                'html_tags': re.compile(r'<[^>]+>'),
+            }
+
 def clear_reviews_from_dataset(labels, list_of_texts: list, neg=0, pos=1, unsup=2, pos_neg=True):
     """
     Clear the data that is not needed
@@ -21,3 +25,43 @@ def clear_reviews_from_dataset(labels, list_of_texts: list, neg=0, pos=1, unsup=
     return reviews_train_without_unsup, labels_without_unsup
 
 
+def get_occurance_in_text(text_data: str, pattern=PATTERNS['html_tags']) -> set:
+    """
+    Returns a list of elements that match the passed pattern
+    :param text_data: text data to search in
+    :param pattern: regex pattern
+    :return: set of elements found in passed text
+    """
+    tags = pattern.findall(text_data)
+
+    return set(tags)
+
+
+def get_occurance_in_dataset(dataset: list, pattern=PATTERNS['html_tags']) -> set:
+    """
+    Returns list of elements that match to the pattern
+    :param pattern: regex pattern
+    :param dataset: list of texts to search in
+    :return: set of elements found in passed texts
+    """
+    list_of_tags = []
+
+    for x in dataset:
+        if isinstance(x, bytes):
+            x = x.decode('utf-8')
+        list_of_tags.extend(get_occurance_in_text(x, pattern))
+
+    return set(list_of_tags)
+
+
+def clear_substr_in_texts(dataset: list, pattern=PATTERNS['html_tags'], replace_with: str = "") -> list:
+    """
+    Removes indicated pattern in text elements
+    :param replace_with: string that should be applied for replacement
+    :param dataset: list of strings or bytes
+    :param pattern: pattern to search
+    :return: list of strings (if the list of bytes is passed they are converted to strings)
+    """
+    return [re.sub(pattern, replace_with,
+                   txt.decode('utf-8') if isinstance(txt, bytes) else txt)
+            for txt in dataset]
