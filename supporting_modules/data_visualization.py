53d75a8e044f558c4bd94ce013b4ae1daf6e2555
KonradKaczmarek79 <konrad.kaczmarek79@gmail.com>
2023-08-26 11:00:39 +0200

    Add visualizations to the notebook

--- supporting_modules/data_visualization.py
+++ supporting_modules/data_visualization.py
@@ -5,8 +5,9 @@ from collections import Counter
 
 def count_per_label(labels, names):
 
-    fig_dimensions = len(labels) * 2 + 1
-    plt.rcParams['figure.figsize'] = (fig_dimensions, fig_dimensions)
+    # length and height of the figure
+    fig_dimensions = len(labels) * 2 + 1, len(labels) + 1
+    plt.rcParams['figure.figsize'] = (fig_dimensions[0], fig_dimensions[1])
     colors = ('Blue', 'Red', 'Green', 'Yellow', 'Black')
     value_quantity_range = tuple(range(len(labels)))
     for label, name, nr in zip(labels, names, value_quantity_range):
@@ -37,11 +38,11 @@ def make_count_histogram(list_of_texts, d=None):
     # numbers that will be bounds of our ranges
     ends_of_ranges = np.arange(min_quantity, max_quantity, d)
 
-    # Zliczenie częstości wystąpień słów
+    # Counting the occurrence frequency of words
     word_counts = Counter(words_per_text)
     k = int(np.round(np.mean((right_boundary, left_boundary))))
 
-    plt.figure(figsize=(16, 4))
+    plt.figure(figsize=(16, 6))
     hist_data = plt.hist(words_per_text, bins=k, width=10, align='mid')
     plt.xticks(list(ends_of_ranges) + [max_quantity], rotation=60, ha='right')
     plt.title('Words per review histogram')
